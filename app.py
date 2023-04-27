e02f89eb50f6dd0cf990355268dcfa6c3b43f56d
zver <zver@zver.ru>
2023-04-27 11:53:19 +0300

    workflows

--- app.py
+++ app.py
@@ -1,6 +1,56 @@
-from flask import Flask
+from hashlib import md5
 
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+
+app = Flask("app")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), unique=True)
+    password = db.Column(db.String(128))
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def index():
+    users = User.query.all()
+    response = {
+        "total": len(users),
+        "users": [{"username": user.username} for user in users],
+    }
+    return jsonify(response)
+
+
+@app.route("/api/register", methods=["POST"])
+def register():
+    user_data = request.json
+    if not user_data or "username" not in user_data or "password" not in user_data:
+        return jsonify({"error": "invalid_request"}), 400
+
+    try:
+        user = User(
+            username=user_data["username"],
+            password=md5(user_data["password"].encode()).hexdigest(),
+        )
+        db.session.add(user)
+        db.session.commit()
+    except IntegrityError:
+        return jsonify({"error": "already_exists"}), 400
+
+    return jsonify({"username": user.username}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 @app.route("/")
@@ -9,4 +59,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
