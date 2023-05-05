7ef6dfdba7a86a51f701f36060da910536a80078
katyssha <118836572+katyssha@users.noreply.github.com>
2023-05-05 19:45:20 +0300

    try again

--- app.py
+++ app.py
@@ -57,7 +57,3 @@ if __name__ == "__main__":
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
